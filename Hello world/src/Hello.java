b248e22f7a8ab247e8a10a8e40675de68ded35cc
MihaiUriciuc <mihaia932@gmail.com>
2021-10-31 09:40:10 +0200

    Change1

--- Hello world/src/Hello.java
+++ Hello world/src/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		/* hello*/ 
+		System.out.println("This is a test");
 		/* hello*/  
 	}
 
